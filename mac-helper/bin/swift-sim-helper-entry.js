12e069b210a0a7590b0681a788be79b7cf21ebf7
Miguel <serram1994@gmail.com>
2026-07-31 12:06:17 +0200

    Load lock ownership guard in helper

--- mac-helper/bin/swift-sim-helper-entry.js
+++ mac-helper/bin/swift-sim-helper-entry.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import "../src/lockOwnershipPreload.js";
 import "../src/ownedWorkerPreload.js";
 import "../src/helperHttpBoundaryPreload.js";
 await import("./swift-sim-helper.js");
