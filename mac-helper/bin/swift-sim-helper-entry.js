0ab480e79eeebe99d972903e0d63485645f89b09
Miguel <serram1994@gmail.com>
2026-07-31 18:55:04 +0200

    Propagate hardened runtime to helper children

--- mac-helper/bin/swift-sim-helper-entry.js
+++ mac-helper/bin/swift-sim-helper-entry.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 import "../src/lockOwnershipPreload.js";
 import "../src/ownedWorkerPreload.js";
+import "../src/runtimeHealthPreload.js";
 import "../src/deviceBuildCapabilityBoundaryPreload.js";
 import "../src/helperHttpBoundaryPreload.js";
 import { installRenewalShutdownGuard } from "../src/renewalShutdownPreload.js";
+import { appendNodeImport } from "../src/runtimePreloadOptions.js";
 
+const hardenedPreloadURL = new URL("../src/hardenedRuntimePreload.js", import.meta.url).href;
+process.env.NODE_OPTIONS = appendNodeImport(process.env.NODE_OPTIONS, hardenedPreloadURL);
 installRenewalShutdownGuard();
 await import("./swift-sim-helper.js");
