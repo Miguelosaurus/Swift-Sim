ba00caec7f4839cb767bf821337b41a5f9614624
Miguel <serram1994@gmail.com>
2026-08-01 22:33:31 +0200

    Preserve default signals for one-shot helper commands

--- mac-helper/bin/swift-sim-helper-entry.js
+++ mac-helper/bin/swift-sim-helper-entry.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 import "../src/commandDeadlinePreload.js";
 import "../src/asyncCommandGroupPreload.js";
-import "../src/helperShutdownDeadlinePreload.js";
 import "../src/atomicLockRemovalPreload.js";
 import "../src/lockOwnershipPreload.js";
 import "../src/ownedWorkerPreload.js";
 import "../src/runtimeHealthPreload.js";
 import "../src/deviceBuildCapabilityBoundaryPreload.js";
 import "../src/helperHttpBoundaryPreload.js";
+import { helperRunsAsService } from "../src/helperShutdownScope.js";
 import { installRenewalShutdownGuard } from "../src/renewalShutdownPreload.js";
 import { installSwiftSimChildRuntimeBoundary } from "../src/swiftSimChildRuntimeBoundary.js";
 
+if (helperRunsAsService()) {
+  await import("../src/helperShutdownDeadlinePreload.js");
+}
 installSwiftSimChildRuntimeBoundary();
 installRenewalShutdownGuard();
 await import("./swift-sim-helper.js");
