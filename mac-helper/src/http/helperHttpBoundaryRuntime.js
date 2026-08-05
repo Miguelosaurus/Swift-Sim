745952a4a7a12ecdfba94413f6a1f088b1ce11c6
Miguel <serram1994@gmail.com>
2026-08-05 16:34:25 +0200

    Extract helper HTTP server runtime

---
+++ mac-helper/src/http/helperHttpBoundaryRuntime.js
@@ -0,0 +1,134 @@
+// @ts-check
+
+/**
+ * @typedef {{ headersSent?: boolean, destroy(error?: Error): unknown }} HelperResponseLike
+ * @typedef {(request: unknown, response: HelperResponseLike) => unknown} RequestListener
+ * @typedef {object | RequestListener | undefined} CreateServerInput
+ * @typedef {(optionsOrListener?: CreateServerInput, listener?: RequestListener) => unknown} CreateServerLike
+ * @typedef {{ createServer: CreateServerLike }} MutableHttpModule
+ * @typedef {{ unref?(): unknown }} IntervalHandle
+ * @typedef {(callback: () => void, intervalMs: number) => IntervalHandle} ScheduleInterval
+ */
+
+/**
+ * Own the compatibility HTTP server wrapper and its maintenance scheduler.
+ * Route authorization, response projection, persistence, and concrete service
+ * construction remain injected responsibilities.
+ */
+export class HelperHttpBoundaryRuntime {
+  /** @type {MutableHttpModule} */
+  #httpModule;
+  /** @type {CreateServerLike} */
+  #originalCreateServer;
+  /** @type {() => void} */
+  #syncBuiltinExports;
+  /** @type {(request: unknown, response: HelperResponseLike) => boolean} */
+  #dispatchRequest;
+  /** @type {(response: HelperResponseLike) => void} */
+  #writeUnavailable;
+  /** @type {(error: unknown) => void} */
+  #reportError;
+  /** @type {() => Promise<unknown>} */
+  #runMaintenance;
+  /** @type {ScheduleInterval} */
+  #scheduleInterval;
+  /** @type {number} */
+  #maintenanceIntervalMs;
+  /** @type {IntervalHandle | undefined} */
+  #maintenanceTimer;
+  #installed = false;
+
+  /**
+   * @param {{
+   *   httpModule: MutableHttpModule,
+   *   originalCreateServer: CreateServerLike,
+   *   syncBuiltinExports(): void,
+   *   dispatchRequest(request: unknown, response: HelperResponseLike): boolean,
+   *   writeUnavailable(response: HelperResponseLike): void,
+   *   reportError(error: unknown): void,
+   *   runMaintenance(): Promise<unknown>,
+   *   scheduleInterval: ScheduleInterval,
+   *   maintenanceIntervalMs: number,
+   * }} options
+   */
+  constructor({
+    httpModule,
+    originalCreateServer,
+    syncBuiltinExports,
+    dispatchRequest,
+    writeUnavailable,
+    reportError,
+    runMaintenance,
+    scheduleInterval,
+    maintenanceIntervalMs,
+  }) {
+    this.#httpModule = httpModule;
+    this.#originalCreateServer = originalCreateServer;
+    this.#syncBuiltinExports = syncBuiltinExports;
+    this.#dispatchRequest = dispatchRequest;
+    this.#writeUnavailable = writeUnavailable;
+    this.#reportError = reportError;
+    this.#runMaintenance = runMaintenance;
+    this.#scheduleInterval = scheduleInterval;
+    this.#maintenanceIntervalMs = maintenanceIntervalMs;
+  }
+
+  install() {
+    if (this.#installed) return;
+    this.#installed = true;
+    const runtime = this;
+    /**
+     * @this {unknown}
+     * @param {CreateServerInput} optionsOrListener
+     * @param {RequestListener | undefined} listener
+     */
+    function guardedCreateServer(optionsOrListener, listener) {
+      const hasListenerOverload = typeof optionsOrListener === "function";
+      const resolvedOptions = hasListenerOverload ? undefined : optionsOrListener;
+      const resolvedListener = hasListenerOverload ? optionsOrListener : listener;
+      const guardedListener = typeof resolvedListener === "function"
+        ? (request, response) => runtime.#handleRequest(request, response, resolvedListener)
+        : undefined;
+      const server = resolvedOptions === undefined
+        ? runtime.#originalCreateServer.call(this, guardedListener)
+        : runtime.#originalCreateServer.call(this, resolvedOptions, guardedListener);
+      runtime.#startMaintenance();
+      return server;
+    }
+    this.#httpModule.createServer = guardedCreateServer;
+    this.#syncBuiltinExports();
+  }
+
+  /**
+   * @param {unknown} request
+   * @param {HelperResponseLike} response
+   * @param {RequestListener} listener
+   */
+  #handleRequest(request, response, listener) {
+    try {
+      if (this.#dispatchRequest(request, response)) return;
+    } catch (error) {
+      this.#reportError(error);
+      if (!response.headersSent) {
+        this.#writeUnavailable(response);
+      } else {
+        response.destroy(error instanceof Error ? error : undefined);
+      }
+      return;
+    }
+    return listener(request, response);
+  }
+
+  #startMaintenance() {
+    if (this.#maintenanceTimer) return;
+    this.#runMaintenancePass();
+    this.#maintenanceTimer = this.#scheduleInterval(() => {
+      this.#runMaintenancePass();
+    }, this.#maintenanceIntervalMs);
+    this.#maintenanceTimer.unref?.();
+  }
+
+  #runMaintenancePass() {
+    void this.#runMaintenance().catch(() => {});
+  }
+}
